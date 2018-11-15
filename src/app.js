c6e995d41b1a548041019bc87df347c8b19a7060
Nikola Krivokapic <nikolakrivokapic84@gmail.com>
2018-11-15 11:37:54 +0100

    Change variable name from arrayOfChars to arrayOfLines

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@ import {arraysEqual, existsRight, notExistsRight, existsLeft, notExistsLeft, exi
 import style from './app.css';
 
 document.getElementById('button').addEventListener('click', () => {
-    const arrayOfChars = document.getElementById('textArea').value.split('\n').map((oneLine) => oneLine.split('')).filter((item) => item.length);
-    const path = getPath(arrayOfChars);
+    const arrayOfLines = document.getElementById('textArea').value.split('\n').map((oneLine) => oneLine.split('')).filter((item) => item.length);
+    const path = getPath(arrayOfLines);
     document.getElementById('resultPath').innerHTML = path;
     document.getElementById('resultLetters').innerHTML = path.replace(/[^A-Z]+/g, '');
 });
 
-function getPath(arrayOfChars) {
-    const startingLineIndex = arrayOfChars.findIndex((line) => line.some((char) => char === '@'));
-    const firstLine = arrayOfChars[startingLineIndex];
+function getPath(arrayOfLines) {
+    const startingLineIndex = arrayOfLines.findIndex((line) => line.some((char) => char === '@'));
+    const firstLine = arrayOfLines[startingLineIndex];
     const startingCharIndex = firstLine.findIndex((char) => char === '@');
 
     let direction;
@@ -20,7 +20,7 @@ function getPath(arrayOfChars) {
         direction = DIRECTIONS.TO_RIGHT;
     } else if (existsLeft(firstLine, startingCharIndex)) {
         direction = DIRECTIONS.TO_LEFT;
-    } else if (existsAtPosition(arrayOfChars[startingLineIndex + 1], startingCharIndex)) {
+    } else if (existsAtPosition(arrayOfLines[startingLineIndex + 1], startingCharIndex)) {
         direction = DIRECTIONS.TO_BOTTOM;
     }
 
@@ -32,17 +32,17 @@ function getPath(arrayOfChars) {
 
         if (lastLine[lastPosition] === 'x') { return accumulatedPath; }
 
-        const lineIndexInArray = arrayOfChars.findIndex((line) => arraysEqual(line, lastLine));
+        const lineIndexInArray = arrayOfLines.findIndex((line) => arraysEqual(line, lastLine));
 
         if ((lastDirection === DIRECTIONS.TO_BOTTOM || (lastDirection === DIRECTIONS.TO_LEFT && notExistsLeft(lastLine, lastPosition)) || (lastDirection === DIRECTIONS.TO_RIGHT && notExistsRight(lastLine, lastPosition)))
-            && lastDirection !== DIRECTIONS.TO_UP && existsAtPosition(arrayOfChars[lineIndexInArray + 1], lastPosition)) {
-            accumulatedPath += arrayOfChars[lineIndexInArray + 1][lastPosition];
-            return recursiveSearch({lastLine: arrayOfChars[lineIndexInArray + 1], lastPosition, lastDirection: DIRECTIONS.TO_BOTTOM, accumulatedPath});
+            && lastDirection !== DIRECTIONS.TO_UP && existsAtPosition(arrayOfLines[lineIndexInArray + 1], lastPosition)) {
+            accumulatedPath += arrayOfLines[lineIndexInArray + 1][lastPosition];
+            return recursiveSearch({lastLine: arrayOfLines[lineIndexInArray + 1], lastPosition, lastDirection: DIRECTIONS.TO_BOTTOM, accumulatedPath});
         }
         else if ((notExistsRight(lastLine, lastPosition) || ((lastDirection === DIRECTIONS.TO_LEFT) && notExistsLeft(lastLine, lastPosition)))
-            && lastDirection !== DIRECTIONS.TO_BOTTOM && existsAtPosition(arrayOfChars[lineIndexInArray - 1], lastPosition)) {
-            accumulatedPath += arrayOfChars[lineIndexInArray - 1][lastPosition];
-            return recursiveSearch({lastLine: arrayOfChars[lineIndexInArray - 1], lastPosition, lastDirection: DIRECTIONS.TO_UP, accumulatedPath});
+            && lastDirection !== DIRECTIONS.TO_BOTTOM && existsAtPosition(arrayOfLines[lineIndexInArray - 1], lastPosition)) {
+            accumulatedPath += arrayOfLines[lineIndexInArray - 1][lastPosition];
+            return recursiveSearch({lastLine: arrayOfLines[lineIndexInArray - 1], lastPosition, lastDirection: DIRECTIONS.TO_UP, accumulatedPath});
         } else if (lastDirection !== DIRECTIONS.TO_LEFT && existsRight(lastLine, lastPosition)) {
             accumulatedPath += lastLine[lastPosition + 1];
             return recursiveSearch({lastLine, lastPosition: lastPosition + 1, lastDirection: DIRECTIONS.TO_RIGHT, accumulatedPath});
